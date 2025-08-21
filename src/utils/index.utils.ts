7580b2b516cb32c8e17f45d91c6e5dd18fbef3f6
skippergoroye <oluwatosinadegoroye21@gmail.com>
2025-08-21 08:33:17 +0100

    feat: class validator added

---
+++ src/utils/index.utils.ts
@@ -0,0 +1,85 @@
+import { Response } from 'express';
+import { createLogger , format , info, transport, transports } from 'winston';
+// import { BANKS } from '../interfaces/enum/payee-enum';
+
+const printRed = (text: string) => {
+  console.log('\x1b[31m%s\x1b[0m', `${text} \n`);
+};
+
+const logger = createLogger({
+  transports :[
+    new transports.File({
+      filename:'./logs/index.log',
+      level:'error',
+      format:format.combine(format.timestamp({format:'YYYY-MM-DD HH:mm:ss'}),format.printf((info)=>`${info.timestamp} ${info.level} : ${info.message} `))
+    })
+  ]
+})
+
+const  escapeHtml = (html:string) => {
+  return html.replace(/[&<>"']/g, '');
+}
+
+
+
+const isEmpty = (data: any) =>{
+  return !data || data.length === 0 || typeof data == 'undefined' || data == null || Object.keys(data).length == 0;
+};
+
+
+
+const handleError = (res: Response, message: string, statusCode: number = 400) => {
+  logger.log({level : 'error' , message});
+  return res.status(statusCode).json({ status: false, message });
+};
+
+const handleSuccess = (res: Response, message: string, data = {}, statusCode: number = 200) => {
+  return res.status(statusCode).json({ status: true, message, data: { ...data } });
+};
+
+const generateCode = (num: number = 15) => {
+  const dateString = Date.now().toString(36);
+  const randomness = Math.random().toString(36).substr(2);
+  let result = randomness + dateString;
+  result = result.length > num ? result.substring(0, num) : result;
+  return result.toUpperCase();
+};
+
+
+const parseToObject = (value: string): any => {
+  let counter = 0;
+  let data = JSON.parse(value);
+  while(counter <= 2){
+    if(typeof data == 'object'){
+      break;
+    }else{
+      data = JSON.parse(data);
+      counter++;
+    }
+  }
+  return data;
+
+}
+
+
+// const getBankName = (bankCode:string): string =>{
+//   const filter = BANKS.filter(item => (item.code == bankCode));
+//   if(filter.length > 0){
+//     return filter[0].name;
+//   }
+//   return '';
+// }
+
+
+const Utility = {
+  printRed,
+  handleError,
+  handleSuccess,
+  generateCode,
+  isEmpty,
+  escapeHtml,
+  parseToObject,
+  // getBankName
+};
+
+export default Utility;
---
+++ src/validator/user-validator-schema.ts
@@ -0,0 +1,41 @@
+import * as yup from 'yup';
+import { AccountStatus } from '../interfaces/enum/user-enum';
+
+
+const registerSchema = yup.object({
+  firstname: yup.string().lowercase().trim().required(),
+  lastname: yup.string().lowercase().trim().required(),
+  email: yup.string().email().lowercase().trim().required(),
+  password: yup.string().min(6).trim().required(),
+});
+
+const loginSchema = yup.object({
+  email: yup.string().email().lowercase().trim().required(),
+  password: yup.string().min(6).trim().required(),
+});
+
+const forgotPasswordSchema = yup.object({
+  email: yup.string().email().lowercase().trim().required(),
+});
+
+const resetPasswordSchema = yup.object({
+  code: yup.string().trim().required(),
+  email: yup.string().email().lowercase().trim().required(),
+  password: yup.string().min(6).trim().required(),
+});
+
+const setAccountStatusSchema = yup.object({
+  userId: yup.string().trim().required(),
+  status: yup.string().required().uppercase().oneOf(Object.values(AccountStatus)),
+});
+
+
+const ValidationSchema = {
+  registerSchema,
+  loginSchema,
+  forgotPasswordSchema,
+  resetPasswordSchema,
+  setAccountStatusSchema
+};
+
+export default ValidationSchema;
